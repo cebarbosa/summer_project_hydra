6f922aef6ed502fb2bf3bc7a52d8a0835e25a361
NatalieGrasser <87664890+NatalieGrasser@users.noreply.github.com>
2021-07-20 13:57:38 +0200

    Update context.py

--- context.py
+++ context.py
@@ -5,5 +5,7 @@ project_name = "summer_project_hydra"
 home_dir= []
 if getpass.getuser() == "kadu":
     home_dir = f"/home/kadu/Dropbox/{project_name}"
+elif getpass.getuser() == "natalie":
+    home_dir = f"/home/natalie/Desktop/files/uni/ESO/{project_name}"
 else:
     raise ValueError("Computer not set up!")
