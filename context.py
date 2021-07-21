3ed9e3b3710f4b340a89bb4d82ab5789d6f9d87e
cebarbosa <kadu.barbosa@gmail.com>
2021-07-21 13:12:52 -0300

    Included matplotlibi/seaborn style.

--- context.py
+++ context.py
@@ -1,5 +1,8 @@
-import os
 import getpass
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 project_name = "summer_project_hydra"
 home_dir= []
@@ -9,3 +12,15 @@ elif getpass.getuser() == "natalie":
     home_dir = f"/home/natalie/Desktop/files/uni/ESO/{project_name}"
 else:
     raise ValueError("Computer not set up!")
+
+# Matplotlib settings
+plt.style.context("seaborn-paper")
+plt.rcParams["text.usetex"] = False
+plt.rcParams["xtick.direction"] = "in"
+plt.rcParams["ytick.direction"] = "in"
+plt.rcParams["xtick.minor.visible"] = True
+plt.rcParams["ytick.minor.visible"] = True
+plt.rcParams["xtick.top"] = True
+plt.rcParams["ytick.right"] = True
+sns.set()
+sns.set_style(rc={"font.family": "serif"})
