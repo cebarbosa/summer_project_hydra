b68d55c7108096b5186adaa6dd7de7dd3b81379c
cebarbosa <kadu.barbosa@gmail.com>
2021-07-27 03:08:41 -0300

    Finalized extraction of spectra for all fields.

--- extract_spectra.py
+++ extract_spectra.py
@@ -1,3 +1,9 @@
+"""
+Determination of optimal aperture and extraction of spectra of systems in the
+halo of NGC 3311
+
+"""
+
 import os
 import warnings
 
@@ -6,10 +12,10 @@ from scipy.signal import argrelextrema
 from astropy.io import fits
 from astropy.table import Table, hstack
 from astropy.stats import sigma_clipped_stats
+from astropy.visualization import hist
 from photutils.aperture import aperture_photometry
 from photutils.aperture import EllipticalAperture, EllipticalAnnulus
 import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
 from tqdm import tqdm
 from astropy.utils.exceptions import AstropyWarning
 
@@ -18,17 +24,24 @@ import misc
 
 warnings.simplefilter('ignore', category=AstropyWarning)
 
-def set_phot_aper(flux, fluxerr, mask, table, n_in=2, n_out=3, smas=None,
-                  field=None, redo=False):
-    if "RADIUS_EXTRACT" in table.columns and not redo:
-        return table
-    aper_phot_plots_dir = os.path.join(os.getcwd(), "extract_apertures")
-    if not os.path.exists(aper_phot_plots_dir):
-        os.mkdir(aper_phot_plots_dir)
+def set_phot_aper(flux, fluxerr, mask, table, n_in=3, n_out=6, field=None,
+                  redo=False):
+    """ Performs aperture photometry to determine best the radius with
+    highest S/N. """
+    outfile = f"radius_extract_n{n_in}_{n_out}_rkron.fits"
+    if os.path.exists(outfile) and not redo:
+        otable = Table.read(outfile)
+        return otable
+    if os.path.exists(outfile):
+        os.remove(outfile)
+    plots_dir = os.path.join(os.getcwd(),
+                          f"radius_extract_n{n_in}_{n_out}_rkron")
+    if not os.path.exists(plots_dir):
+        os.mkdir(plots_dir)
     field = "fieldA" if field is None else field
     r_extract = []
     desc = "Determining max S/N apertures"
-    nkr = np.linspace(0.1, 1, 50)
+    nkr = np.linspace(0.1, n_in, 50)
     for i, t in enumerate(tqdm(table, desc=desc)):
         objid = f"{field}_{t['NUMBER']:03d}"
         # Setting the geometry of ellipses
@@ -52,7 +65,8 @@ def set_phot_aper(flux, fluxerr, mask, table, n_in=2, n_out=3, smas=None,
         # unmask the source itself from the general mask
         objmask = apertures[-1].to_mask(method="center").to_image(
                                           flux.shape).astype(np.float)
-        source_mask = np.logical_or(mask - objmask > 0, np.isnan(flux))
+        source_mask = (mask - objmask == 0).astype(np.int)
+        # Performing the photometry inside the source
         phot_table = aperture_photometry(flux, apertures, error=fluxerr,
                                          mask=source_mask)
         phot = np.array([phot_table[f"aperture_sum_{i}"].data[0] for i in
@@ -66,11 +80,14 @@ def set_phot_aper(flux, fluxerr, mask, table, n_in=2, n_out=3, smas=None,
                             a_out=n_out * rkron, b_out=n_out * rkron * ar,
                             b_in=n_in * rkron * ar, theta=theta)
         annmask = annulus_aperture.to_mask(method='center')
-        annmask_img = annmask.to_image(flux.shape)
-        usemask = (mask == 0)
-        goodpix = annmask.multiply(usemask)
         annulus_data = annmask.multiply(flux)
-        annulus_data_1d = annulus_data[goodpix == 1]
+        annulus_mask = annmask.multiply(source_mask)
+        # plt.subplot(1,2,1)
+        # plt.imshow(annulus_data, origin="lower")
+        # plt.subplot(1,2,2)
+        # plt.imshow(annulus_mask, origin="lower")
+        # plt.show()
+        annulus_data_1d = annulus_data[annulus_mask==1]
         bg_mean, bg_med, bg_std = sigma_clipped_stats(annulus_data_1d)
         background = bg_med * areas
         source = phot - background
@@ -89,166 +106,150 @@ def set_phot_aper(flux, fluxerr, mask, table, n_in=2, n_out=3, smas=None,
         plt.ylabel("S/N")
         plt.legend()
         plt.tight_layout()
-        plt.savefig(os.path.join(aper_phot_plots_dir,
+        plt.savefig(os.path.join(plots_dir,
                                  f"apphot_{objid}.png"), dpi=200)
         plt.close()
-    t = Table([r_extract], names=["r_extract"])
+    t = Table([r_extract], names=["RADIUS_SN_MAX"])
     new_table = hstack([table, t])
+    new_table.write(outfile, overwrite=True)
     return new_table
 
-def select_candidates(table, seeing, dflux_radius=0.5, field=None, redo=False,
-                      ellip_max=0.3):
-    """ Narrow the number of GC candidates according to their morphology. """
-    outtable = os.path.join(os.getcwd(), "gc_candidates.fits")
-    if os.path.exists(outtable) and not redo:
-        table = Table.read(outtable)
-        return table
-    title = f"Field {field[-1]}"
-    table = table[table["FLUX_RADIUS"] > 0]
-    table =  table[table["FLUX_RADIUS"] < 5]
-    candidates = table[table["FLUX_RADIUS"] < 4]
-    # Remove outliers in flux
-    mags = candidates["MAG_AUTO"]
-    mmean, mmedian, mstd = sigma_clipped_stats(mags)
-    candidates = candidates[np.abs(mags - mmedian) <= 5 * mstd]
-    # Use only round sources
-    ellip = candidates["ELLIPTICITY"]
-    candidates = candidates[ellip <= ellip_max]
-    # Use only sources with flux_radius similar to point sources
-    flux_radius = candidates["FLUX_RADIUS"]
-    candidates = candidates[np.abs(flux_radius - seeing) <= dflux_radius]
-    candidates.write(outtable, overwrite=True)
-    # Plotting results
-    fig = plt.figure(figsize=(context.fig_width, 2.5))
-    ax = plt.subplot(111)
-    ax.grid(True)
-    idx = [i for i, n in enumerate(table["NUMBER"])
-             if n not in candidates["NUMBER"]]
-    ax.plot(table["FLUX_RADIUS"][idx], table["MAG_AUTO"][idx], "x",
-             label=r"$\varepsilon > {}$".format(ellip_max))
-    ax.plot(candidates["FLUX_RADIUS"], candidates["MAG_AUTO"], ".",
-             label=r"$\varepsilon \leq {}$".format(ellip_max))
-    # ymin = candidates["MAG_AUTO"].max() + 1
-    # ymax = candidates["MAG_AUTO"].min() - 1
-    # ax.set_ylim(ymin, ymax)
-    ax.set_ylim(ax.get_ylim()[::-1])
-    # Including rectangle
-    ymin = candidates["MAG_AUTO"].min() - 0.5
-    ymax = candidates["MAG_AUTO"].max() + 0.5
-    xmin = seeing - dflux_radius
-    rect=mpatches.Rectangle((xmin, ymin), 2 * dflux_radius, ymax - ymin,
-                            fill=False, color ="purple", linewidth = 2)
-    plt.gca().add_patch(rect)
-    plt.legend(title=title)
-    plt.xlabel("Flux radius (arcsec)")
-    plt.ylabel("mag (arbitrary units)")
-    plt.subplots_adjust(left=0.1, top=0.99, bottom=0.13, right=0.99)
-    plt.savefig(f"gc_candidates_{field}.png", dpi=250)
-    plt.close()
-    return candidates
-
-def plot_apertures(data, table, n_in=5, n_out=10, redo=True):
-    outimg = f"apertures_nin{n_in}_nout{n_out}.png"
+def plot_apertures(bkgimg, table, redo=False, n_in=3, n_out=6):
+    """ Plot the apertures used for photometry in all cases on top of a
+    background image. """
+    outimg = f"apertures_n{n_in}_{n_out}_rkron.png"
     if os.path.exists(os.path.join(os.getcwd(), outimg)) and not redo:
         return
-    vmin = np.nanpercentile(data, 10)
-    vmax = np.nanpercentile(data, 98)
+    vmin = np.nanpercentile(bkgimg, 10)
+    vmax = np.nanpercentile(bkgimg, 98)
+    # Impute median values for non-defined cases
+    rsnmax = table["RADIUS_SN_MAX"]
+    rsnmax_med = np.nanmedian(rsnmax)
+    table["RADIUS_SN_MAX"][~np.isfinite(rsnmax)] = rsnmax_med
+    # Impute values for the Kron Radius
+    rkron = table["KRON_RADIUS"]
+    rkron_med = np.nanmedian(rkron)
+    table["KRON_RADIUS"][rkron <= 0] = rkron_med
+    # Starting the figure with the background image
     fig = plt.figure(figsize=(8, 7.8))
     ax = plt.subplot(111)
     ax.grid(False)
-    ax.imshow(data, vmin=vmin, vmax=vmax, origin="lower")
+    ax.imshow(bkgimg, vmin=vmin, vmax=vmax, origin="lower", cmap="viridis_r")
+    # Plot all sources in catalog
     for t in table:
         # Setting the geometry of ellipses
         x0 = t["X_IMAGE"] -1
         y0 = t["Y_IMAGE"] - 1
         ar = t["B_IMAGE"] / t["A_IMAGE"]
-        rkron = t["KRON_RADIUS"]
         theta = np.deg2rad(t["THETA_IMAGE"])
-        r_sn_max = t["r_extract"]
+        r_sn_max = t["RADIUS_SN_MAX"]
+        rkron = t["KRON_RADIUS"]
         aper = EllipticalAperture((x0, y0), r_sn_max, r_sn_max * ar,
                                   theta=theta)
-        aper_n_in = EllipticalAperture((x0, y0), n_in,
-                                       n_in * ar, theta=theta)
-        aper_n_out = EllipticalAperture((x0, y0), n_out,
-                                       n_out * ar, theta=theta)
-        aper.plot(lw=0.5)
-        aper_n_in.plot(ls="--", lw=0.5)
-        aper_n_out.plot(ls="--", lw=0.5)
+        aper_n_in = EllipticalAperture((x0, y0), n_in * rkron,
+                                       rkron * ar * n_in, theta=theta)
+        aper_n_out = EllipticalAperture((x0, y0), n_out * rkron,
+                                       rkron * ar * n_out, theta=theta)
+        aper.plot(lw=0.7, color="r")
+        aper_n_in.plot(ls="--", lw=0.7, color="r")
+        aper_n_out.plot(ls="--", lw=0.7, color="r")
     plt.subplots_adjust(left=0.05, bottom=0.05, right=0.99, top=0.99)
     plt.savefig(outimg, dpi=300)
     plt.show()
     plt.close()
 
-def extract_spectra(wave, flux, cube, table, mask, n_mask=1):
-    """ Extract the spectra from the cubes. """
+def extract_spectra_from_cube(cubename, flux, table, mask, n_in=2.5,
+                              n_out=4, field=None, extname=None):
+    """ Extract the spectra from the datacubes. """
+    specs_dir = os.path.join(os.getcwd(),
+                      f"spec1D_n{n_in}_{n_out}_rkron")
+    if not os.path.exists(specs_dir):
+        os.mkdir(specs_dir)
+    field = "field" if field is None else field
     mask = mask.astype(int)
-    zdim, ydim, xdim = cube.shape
-    xpix = np.arange(xdim) + 1
-    ypix = np.arange(ydim) + 1
-    for t in table:
-        # Setting the geometry of ellipses
-        x0 = t["X_IMAGE"] -1
-        y0 = t["Y_IMAGE"] - 1
-        ar = t["B_IMAGE"] / t["A_IMAGE"]
-        rkron = t["KRON_RADIUS"]
-        theta = np.deg2rad(t["THETA_IMAGE"])
-        r_sn_max = t["r_extract"] if np.isfinite(t["r_extract"]) else rkron
+    # Impute median values for non-defined cases
+    rsnmax = table["RADIUS_SN_MAX"]
+    rsnmax_med = np.nanmedian(rsnmax)
+    table["RADIUS_SN_MAX"][~np.isfinite(rsnmax)] = rsnmax_med
+    # Impute values for the Kron Radius
+    rkron = table["KRON_RADIUS"]
+    rkron_med = np.nanmedian(rkron)
+    table["KRON_RADIUS"][rkron <= 0] = rkron_med
+    desc1 = f"Processing {field} "
+    desc2 = f"Extracted source"
+    wave = misc.array_from_header(cubename)
+    outdata = np.zeros((2, len(table), 2, len(wave)))
+    for i, extname in enumerate(tqdm(["DATA", "STAT"], desc1)):
+        cube = fits.getdata(cubename, ext=i+1)
+        for j, t in enumerate(tqdm(table, desc=desc2)):
+            # Setting the geometry of ellipses
+            x0 = t["X_IMAGE"] -1
+            y0 = t["Y_IMAGE"] - 1
+            ar = t["B_IMAGE"] / t["A_IMAGE"]
+            rkron = t["KRON_RADIUS"]
+            theta = np.deg2rad(t["THETA_IMAGE"])
+            r_sn_max = t["RADIUS_SN_MAX"]
+            # Defining apertures for the photometry without the correct mask
+            aper = EllipticalAperture((x0, y0), r_sn_max, r_sn_max * ar,
+                                      theta=theta)
+            annulus_aperture = EllipticalAnnulus((x0, y0), a_in=n_in * rkron,
+                        a_out=n_out * rkron, b_out=n_out * rkron * ar,
+                        b_in=n_in * rkron * ar, theta=theta)
+            # unmask the source itself from the general mask
+            objmask = aper.to_mask(method="center").to_image(
+                                              flux.shape).astype(np.float)
+            source_mask = (mask - objmask == 0).astype(np.int)
+            for k, region in enumerate([aper, annulus_aperture]):
+                region_mask = region.to_mask(method='center').to_image(
+                                              flux.shape).astype(np.float)
+                # get x and y indices where the source is located
+                idy, idx = np.where(region_mask == 1)
+                # Extracting spectra from cube
+                specs = cube[:, idy, idx]
+                # Removing masked regions
+                specmask = source_mask[idy, idx]
+                specs = specs[:, specmask==1]
+                spec1D = np.nanmean(specs, axis=1)
+                outdata[i, j, k, :] = spec1D
+    # Producing output tables
+    print("Saving output tables")
+    colnames = ["wave", "flux_source", "fluxerr_source", "flux_annulus",
+                "fluxerr_annulus"]
+    for j, t in enumerate(table):
+        output = os.path.join(os.getcwd(), specs_dir,
+                              f"{field}_{t['NUMBER']:03d}.fits")
+        t = Table([wave, outdata[0, j, 0, :], np.sqrt(outdata[1, j, 0, :]),
+                   outdata[0, j, 1, :], np.sqrt(outdata[1, j, 1, :])],
+                  names=colnames)
+        t.write(output, overwrite=True)
 
-        aper = EllipticalAperture((x0, y0), r_sn_max, r_sn_max * ar,
-                                  theta=theta)
-        annulus_aperture = EllipticalAnnulus((x0, y0), a_in=n_in * rkron,
-                    a_out=n_out * rkron, b_out=n_out * rkron * ar,
-                    b_in=n_in * rkron * ar, theta=theta)
-        # unmask the source itself from the general mask
-        aper_n_mask = EllipticalAperture((x0, y0), n_mask * rkron, n_mask *
-                                       rkron * ar, theta=theta)
-        source_mask = mask - aper_n_mask.to_mask(method="center").to_image(
-                                          flux.shape).astype(np.float)
-        for i, region in enumerate([aper, annulus_aperture]):
-            phot_mask = region.to_mask(method="center").to_image(
-                                         flux.shape).astype(np.float)
-            # get x and y indices where the source is located
-            idy, idx = np.where(phot_mask != 0)
-            # Extracting spectra from cube
-            specs = cube[:, idy, idx]
-            # Removing masked regions
-            specmask = source_mask[idy, idx]
-            specs = specs[:, specmask==0]
-            spec1D = np.nanmean(specs, axis=1)
-            plt.plot(wave, spec1D)
-        plt.show()
 
-if __name__ == "__main__":
-    redo = True
-    stars_rh = [0.65, 0.66, 0.75, 1.]
+def run(n_in=2.5, n_out=4, redo=False):
+    """ Pipeline to run all routines of extranction from data of NGC 3311. """
     for i, field in enumerate(context.fields):
-        seeing = context.seeing[i]
         wdir = os.path.join(context.home_dir, f"data/{field}")
         os.chdir(wdir) # Changing to working directory
-        table = Table.read("sexcat.fits")
-        # Converting table to arcsec
-        table["FLUX_RADIUS"] *= context.PS
-        # Offset for magnitude
-        table["MAG_AUTO"] += 25
+        table = Table.read("source-catalog.fits")
         snimage = f"sn_{field}.fits"
         flux = fits.getdata(snimage)
         fluxerr = fits.getdata(snimage, ext=2)
         # Load mask with all detected sources
         mask = fits.getdata("sources_mask.fits")
-        table = set_phot_aper(flux, fluxerr, mask, table, redo=redo)
-
-        # Select systems that are more likely GC candidates
-        candidates = select_candidates(table, seeing / 2, field=field,
-                                          redo=redo)
-
-
+        # Load mask with objects masked by hand
+        mask_regions = fits.getdata("mask_regions.fits")
+        # Combine the two masks
+        mask[np.isnan(mask_regions)] += 1
         # Calculate the best aperture for extraction
+        table = set_phot_aper(flux, fluxerr, mask, table, redo=redo,
+                              field=field, n_in=n_in, n_out=n_out)
         # Producing a finding chart for the sources
         detection = fits.getdata("detection_masked.fits")
-        plot_apertures(detection, candidates)
-        continue
-        # Extracting the spectra in the apertures
-        cubename = "NGC3311_FieldA_DATACUBE_COMBINED.fits"
-        wave = misc.array_from_header(cubename)
-        cube = fits.getdata(cubename)
-        extract_spectra(wave, flux, cube, table, mask)
+        plot_apertures(detection, table, n_in=n_in, n_out=n_out, redo=redo)
+        # Extracting the spectra in the aperturesn_in=n_in, n_out=n_out
+        cubename = f"NGC3311_Field{field[-1]}_DATACUBE_COMBINED.fits"
+        extract_spectra_from_cube(cubename, flux, table, mask,
+                                  n_in=n_in, n_out=n_out, field=field)
+
+
+if __name__ == "__main__":
+    run()
